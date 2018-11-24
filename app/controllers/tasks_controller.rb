863ac3bc6c7e2bfdcabd977fa913e6d90758614b
kierra-addison <vinediezca@gmail.com>
2018-11-24 11:24:14 +0800

    Style container min-height & order task iteratiion

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = Task.all.order(created_at: :asc)
   end
 
   # GET /tasks/1
